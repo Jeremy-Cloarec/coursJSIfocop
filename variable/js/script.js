1ce8613bbffed2b9042e3d2ca257221cab354098
Jérémy <jeremycloarec@msn.com>
2022-12-15 09:51:47 +0100

    first commit

---
+++ variable/js/script.js
@@ -0,0 +1,16 @@
+"use strict"; //Sans erreur possible
+
+//variable
+//permet de stocker des infos. On utilise let puis on lui donne un  nom.
+
+let pseudo;
+pseudo="Bonjour console";
+console.log(pseudo);
+console.log(typeof pseudo);
+
+let age = 30;
+console.log(age);
+console.log(typeof age);
+let nbrDecimal = 25.5;
+console.log(nbrDecimal);
+console.log(typeof nbrDecimal);
